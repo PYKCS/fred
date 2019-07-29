7e3db867b09afa28e201c0ea7cae2693ed88d981
PyknoDEV <53413231+PyknoDEV@users.noreply.github.com>
2019-07-29 15:19:41 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,30 +1,43 @@
 const Discord = require('discord.js');
-const PREFIX = "!"
+const PREFIX = "?"
+const TOKEN = "NjA0ODM1MzYyNTIyNzkxOTQ2.XT7Daw.dN7bBVjW4OPbUPIfQHxS64KFlZk"
 //const YTDL = require("ytdl-core");
 
 var client = new Discord.Client();
-
 var servers = {};
-//var botch = bot.channels.find(name => name === "bot");
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
-  if(message.author.equals(bot.user)) return;
-  //if(!message.content.startsWith(PREFIX)) return;
-  //var args = message.content.substring(PREFIX.length).split(" ");
-  if(message.content == "hello"){
-    message.channel.send("hi");
+client.on('guildMemberAdd', member => {
+  const logch = member.guild.channels.find(ch => ch.name = "log");
+  const roleN = member.guild.roles.find(role => role.name = "EMen");
+  if(!logch) return;
+  logch.send(`Welcome to the server, ${member}`);
+  member.addRole(roleN)
+    .then(console.log)
+    .catch(console.error);
+})
+
+client.on('message', async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(PREFIX) !== 0) return;
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const botch = message.guild.channels.find(ch => ch.name === 'bot');
+
+  switch(command){
+    case "help":
+      botch.send(`${message.author}\n ?ping - latency test`);
+      break;
+    case "ping":
+      const m = await botch.send("Ping?");
+      m.edit(`${message.author} Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+      break;
+    default:
+      botch.send(`${message.author} - Invalid command, check ?help for the commands.`);
   }
-  //switch(args[0].toLowerCase()){
-  //  case "ping":
-  //    botch.send("Pong.");
-  //    break;
-  //  default:
-  //    botch.send("Invalid command, type !help for a list of commands.");
-  //}
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(TOKEN);
