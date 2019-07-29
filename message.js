3e8f3721206c71d1a3442829389742afc58283b6
PyknoDEV <53413231+PyknoDEV@users.noreply.github.com>
2019-07-29 17:10:31 -0400

    Add files via upload

---
+++ message.js
@@ -0,0 +1,25 @@
+module.exports = (client, message) => {
+  const PREFIX = "?"
+  // Ignore all bots
+  if (message.author.bot) return;
+
+  // Ignore messages not starting with the prefix (in config.json)
+  if (message.content.indexOf(PREFIX) !== 0) return;
+
+  // Our standard argument/command name definition.
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // Grab the command data from the client.commands Enmap
+  const cmd = client.commands.get(command);
+
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) {
+    message.channel.send(`${message.author} - Invalid command (${message}), check ?help for the commands.`);
+    return;
+  }
+
+  // Run the command
+  cmd.run(client, message, args);
+  message.delete();
+};
