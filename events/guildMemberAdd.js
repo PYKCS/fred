308c7f24cc7b5cc5198e45f2f39cd38cc8b88c83
PyknoDEV <53413231+PyknoDEV@users.noreply.github.com>
2019-08-01 22:40:17 -0400

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,5 @@
 module.exports = (client, member) => {
-  const logch = member.guild.channels.find(ch => ch.name = "log");
+  const logch = client.channels.find(ch => ch.name === 'log');
   const roleN = member.guild.roles.find(role => role.name = "EMen");
-  defaultChannel.send(`Welcome ${member.user} to this server.`).catch(console.error);
-  if(!logch) return;
-  logch.send(`Welcome to the server, ${member}`);
-  member.addRole(roleN)
-    .then(console.log)
-    .catch(console.error);
+  member.addRole(roleN);
 }
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async client => {
-  client.user.setActivity("Porn", {
+  client.user.setActivity("jj", {
   type: "WATCHING"
 });
   console.log('I am ready!');
